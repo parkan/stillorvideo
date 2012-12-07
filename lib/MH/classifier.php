0e39a85314602f8f93f314581cea256cdf8ede68
Arkadiy Kukarkin <arkadiy@hypem.com>
2012-12-06 22:22:29 -0500

    Classifier class
    -package comparison functionality into a more general classifier class
    -calculate sum and variance of frame differences

---
+++ lib/MH/classifier.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace MH;
+
+class Classifier {
+    private $_distancesAll;
+    function distancesAll(){
+    // array of distances between available frames
+        if(empty($self->distancesAll)){
+            $distancesAll = [];
+            foreach(array_keys($this->cvec) as $i){
+                for($j = 1; $j + $i < count($this->cvec); $j++){
+                    $distancesAll[] = puzzle_vector_normalized_distance($this->cvec[$i],$this->cvec[($i + $j)]);
+                }
+            }
+            $this->_distancesAll = $distancesAll;
+        }
+
+        return $this->_distancesAll;
+    }
+
+    function distanceSum(){
+    // sum of differences
+        return array_sum($this->distancesAll());
+    }
+
+    function distanceVariance(){
+    // variance of frame differences
+        return stats_variance($this->distancesAll(), true); // 2nd arg must be true for classical variance
+    }
+
+    function distanceThreshCount(){
+    // count of thresholds exceeded
+        $over_thresh = 0;
+        foreach($self->distancesAll() as $d){
+            $over_thresh += ($d > PUZZLE_CVEC_SIMILARITY_THRESHOLD ? 1 : 0);
+        }
+        return $over_thresh;
+    }
+}
+
+class YTThumbClassifier extends Classifier {
+    private $cvec;
+    private $base_url = "http://img.youtube.com/vi/%s/%u.jpg"; // TODO: implement this via API instead of uri substitution (?)
+
+    function getThumbCvec($i){
+        // get thumb
+        $url = sprintf($base_url, $yt_videoid, $i);
+        $ch = curl_init($url);
+        $fn = "/tmp/" . $yt_videoid . "_" . $i . ".jpg";
+        $fp = fopen($fn, "w");
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
+
+        if($i === 0){ // 1st one is HQ
+            system("convert -resize 120x90 $fn $fn"); // lazy!
+        }
+
+        // calculate vectors
+        $this->cvec[] = puzzle_fill_cvec_from_file($fn);
+        unlink($fn);
+    }
+
+    function __counstruct($yt_videoid){
+        $this->cvec = [];
+        foreach([0,1,2,3] as $i){
+            $this->getThumbCvec($i);
+        }
+    }
+}
+
+class ExtraThumbClassifier extends Classifier {
+    function __construct($yt_videoid){
+        $this->cvec = [];
+        // TODO: $this->getVideo();
+        // TODO: while($this->getFrame()){}
+        // ...
+    }
+}
