d34e0ff00456b6c30ec951da4a67dcc34f27b2cd
Arkadiy Kukarkin <arkadiy@hypem.com>
2013-11-03 01:13:19 -0400

    Use OO approach

--- lib/MH/Classifier.php
+++ lib/MH/Classifier.php
@@ -2,11 +2,13 @@
 
 namespace MH;
 
-class Classifier {
+abstract class Classifier {
     private $_distancesAll;
+    protected $cvec;
+
     function distancesAll(){
     // array of distances between available frames
-        if(empty($self->distancesAll)){
+        if(empty($this->distancesAll)){
             $distancesAll = [];
             foreach(array_keys($this->cvec) as $i){
                 for($j = 1; $j + $i < count($this->cvec); $j++){
@@ -32,7 +34,7 @@ class Classifier {
     function distanceThreshCount(){
     // count of thresholds exceeded
         $over_thresh = 0;
-        foreach($self->distancesAll() as $d){
+        foreach($this->distancesAll() as $d){
             $over_thresh += ($d > PUZZLE_CVEC_SIMILARITY_THRESHOLD ? 1 : 0);
         }
         return $over_thresh;
@@ -40,14 +42,14 @@ class Classifier {
 }
 
 class YTThumbClassifier extends Classifier {
-    private $cvec;
     private $base_url = "http://img.youtube.com/vi/%s/%u.jpg"; // TODO: implement this via API instead of uri substitution (?)
+    private $yt_videoid;
 
     function getThumbCvec($i){
         // get thumb
-        $url = sprintf($base_url, $yt_videoid, $i);
+        $url = sprintf($this->base_url, $this->yt_videoid, $i);
         $ch = curl_init($url);
-        $fn = "/tmp/" . $yt_videoid . "_" . $i . ".jpg";
+        $fn = "/tmp/" . $this->yt_videoid . "_" . $i . ".jpg";
         $fp = fopen($fn, "w");
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -60,7 +62,8 @@ class YTThumbClassifier extends Classifier {
         unlink($fn);
     }
 
-    function __counstruct($yt_videoid){
+    function __construct($yt_videoid){
+        $this->yt_videoid = $yt_videoid;
         $this->cvec = [];
         foreach([0,1,2,3] as $i){
             $this->getThumbCvec($i);
